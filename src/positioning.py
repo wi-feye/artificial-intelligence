387626ee3fbac5132dbca600fb1eb95b7837e067
Giuseppe Lombardi <g.lombardi11@studenti.unipi.it>
2022-11-07 15:01:10 +0100

    fixed imports

--- src/positioning.py
+++ src/positioning.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import datetime
-import os.path
 from os import path
+import requests
 
 def devices(URL_DEVICES, headers):
     req = requests.get(URL_DEVICES, headers=headers)
