b69bad081e04e523e15e27cbde7073c49c873f2d
xiaoli <1998619xiaoli@gmail.com>
2022-11-10 22:06:38 +0100

    minor fix

--- src/utils.py
+++ src/utils.py
@@ -45,7 +45,7 @@ def activeness_index(df, start=None, stop=None):
     times: how many times generate the time
     when: if not defined it will generate time from 1H before
     """
-def generate_time_every(freq="1min", times=100, when=None):
+def generate_time_every(df, freq="1min", times=100, when=None):
     if when == None:
         when = (datetime.datetime.now() - datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S")
     
@@ -54,3 +54,5 @@ def generate_time_every(freq="1min", times=100, when=None):
     for i in range(len(ids)-1):
         df_intervals.append(time_interval(df, ids[i].strftime("%Y-%m-%d %H:%M:%S"),
                                             ids[i+1].strftime("%Y-%m-%d %H:%M:%S")))
+    
+    return df_intervals
