12bf4a1968d35253b2e982acdf0cbe31cb697101
Giuseppe Lombardi <g.lombardi11@studenti.unipi.it>
2022-11-22 02:12:47 +0100

    New module prediction added and new experiments in Fibonacci
    New functions that perform sampling and compute likelihood, that it can give us points of interest.

--- src/mapping.py
+++ src/mapping.py
@@ -19,9 +19,11 @@ def geo_building() -> gpd.GeoDataFrame:
         gpd.GeoDataFrame: geodataframe for the building. Each record is a space (area or room) with a 'geometry' feature (polygon)
     """
 
-    building = Building()
+    par = Parameter()
+    buildings = Building()
+    building = buildings.dictionary[par.select_building]
 
-    gdf_building = gpd.GeoDataFrame(building.dictionary)
+    gdf_building = gpd.GeoDataFrame(building)
     gdf_building['geometry'] = gdf_building['geometry'].apply(lambda x : Polygon(x))
 
     return gdf_building
@@ -34,13 +36,15 @@ def geo_sniffers() -> gpd.GeoDataFrame:
     """
 
     par = Parameter()
+    buildings = Building()
+    sniffers_list = buildings.sniffers_list[par.select_building]
 
     sniffers_index = []
-    for i in range(len(par.sniffers_list)):
+    for i in range(len(sniffers_list)):
         sniffers_index.append('sniffer_'+str(i+1))
 
     gdf_sniffers = gpd.GeoDataFrame({'sniffer':sniffers_index})
-    gdf_sniffers[["x", "y"]] = gpd.GeoDataFrame(par.sniffers_list)
+    gdf_sniffers[["x", "y"]] = gpd.GeoDataFrame(sniffers_list)
     gdf_sniffers['geometry'] = gpd.points_from_xy(gdf_sniffers['x'], gdf_sniffers['y'])
 
     return gdf_sniffers
@@ -57,7 +61,9 @@ def geo_points(rssi_df: pd.DataFrame) -> gpd.GeoDataFrame:
 
     gdf_points = gpd.GeoDataFrame(rssi_df)
     gdf_points['geometry'] = gpd.points_from_xy(gdf_points['x'], gdf_points['y'])
-    gdf_points['timestamp'] = gdf_points['timestamp'].astype(str)
+
+    if('timestamp' in rssi_df.columns):
+        gdf_points['timestamp'] = gdf_points['timestamp'].astype(str)
 
     return gdf_points
 
@@ -80,7 +86,10 @@ def map(rssi_df: pd.DataFrame):
     map_sniffers = gdf_sniffers.explore(m=map_building, color='red')
     map_points = gdf_points.explore(m=map_sniffers)
 
+    map_points.save('./plot/map.html')
+
     return map_points.to_json()
+
 
 def heatmap(rssi_df: pd.DataFrame):
     """Create the heatmap in json format
@@ -94,9 +103,13 @@ def heatmap(rssi_df: pd.DataFrame):
 
     gdf_building = geo_building()
     map_heat = gdf_building.explore(style_kwds={'color':'black','weight':3,'fillColor':'gray','fillOpacity':0.2})
+    # Warning!: heatmap take points as (longitude, latitude) so pass it (y,x)
     HeatMap(rssi_df[['y', 'x']].values).add_to(map_heat)
 
+    map_heat.save('./plot/heatmap.html')
+
     return map_heat.to_json()
+
 
 def heatmap_tt(rssi_df: pd.DataFrame):
     """Create the dynamic heatmap in json format
@@ -111,22 +124,23 @@ def heatmap_tt(rssi_df: pd.DataFrame):
     gdf_building = geo_building()
     gdf_sniffers = geo_sniffers()
 
-    map_building = gdf_building.explore(
+    map_heat_tt = gdf_building.explore(
         style_kwds={'color': 'black', 'weight': 3, 'fillColor': 'gray', 'fillOpacity': 0.2})
-    map_sniffers = gdf_sniffers.explore(m=map_building, color='red')
+    map_heat_tt = gdf_sniffers.explore(m=map_heat_tt, color='red')
 
     # List comprehension to make out list of lists
-    heat_data = [[[row['x'], row['y']] for index, row in rssi_df[rssi_df['timestamp'] == i].iterrows()] for i in
-                 rssi_df["timestamp"].unique()]
-
+    # Warning!: heatmap take points as (longitude, latitude) so pass it (y,x)
+    heat_data = [[[row['y'], row['x']] for _, row in rssi_df[rssi_df['timestamp'] == i].iterrows()] for i in
+                rssi_df["timestamp"].unique()]
 
     # Plot it on the map
     hm = folium.plugins.HeatMapWithTime(heat_data, auto_play=True, max_opacity=0.8)
-    hm.add_to(map_sniffers)
+    hm.add_to(map_heat_tt)
 
-    #map_sniffers.save('/home/profeta/Scrivania/map.html')
+    map_heat_tt.save('./plot/heatmap_tt.html')
 
-    return map_sniffers.to_json()
+    return map_heat_tt.to_json()
+
 
 def trajectories(rssi_df: pd.DataFrame):
     """Create the trajectories' map in json format
@@ -143,9 +157,10 @@ def trajectories(rssi_df: pd.DataFrame):
     gdf_points = geo_points(rssi_df)
     map_trajectories = gdf_points.explore(column='mac',m=map_building, cmap='tab20b', legend=False)
 
-    #map_trajectories.save('/home/profeta/Scrivania/traj.html')
+    map_trajectories.save('./plot/traj.html')
 
     return map_trajectories.to_json()
+
 
 def apply_map_tt(rssi_df: pd.DataFrame, mapf, freq="2H", times=5, when: datetime.datetime = None):
     """_summary_
