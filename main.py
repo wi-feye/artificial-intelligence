6937cfdc40c9d1fd720fe8b8194c68dcb9f3d96e
Domenico Ferraro <ferraro.domenico125@gmail.com>
2022-11-23 18:45:52 +0100

    Remove some comments

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ CRON_SECONDS = int(config['CRON_SECONDS'])
 
 def main():
     BASEDATA = WIFEYE_BASEURL_STORAGE
-    print(BASEDATA)
     res = get(f'{BASEDATA}/api/details/ai/')
-    print(res)
     buildings = res.json()
     position_detections = []
     for building in buildings:
@@ -26,9 +24,6 @@ def main():
             detection['timestamp'] = raw['timestamp']
         position_detections += result
     res = post(f'{BASEDATA}/api/ai/create-position-detections/', json=position_detections)
-    print(res)
-    print(res.json())
-
 
 if __name__ == '__main__':
     schedule.every(CRON_SECONDS).seconds.do(main)
