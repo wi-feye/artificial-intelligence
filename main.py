abd371e0eae1b7da531ed9fa05e5755fcc65481b
Giuseppe Lombardi <g.lombardi11@studenti.unipi.it>
2022-11-24 13:11:19 +0100

    Big Clean

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 import json
 from requests import get, post
 from dotenv import dotenv_values
-from final_class import Positioning
+from src.positioning import Positioning
 import schedule
 from time import sleep
+import pandas as pd
 
 config = dotenv_values('env_file')
 WIFEYE_BASEURL_STORAGE = config['WIFEYE_BASEURL_STORAGE']
 CRON_SECONDS = int(config['CRON_SECONDS'])
 
-def main():
-    # BASEDATA = WIFEYE_BASEURL_STORAGE
-    # res = get(f'{BASEDATA}/api/details/ai/')
-    # buildings = res.json()
-    import json
-    with open('./raw_data.json', 'r') as file:
-        buildings = json.load(file)
+def fromDfToJson(df: pd.DataFrame) -> json:
+    df["id"] = df.index
+    return df.to_dict(orient="records")
+
+def jsonShit(buildings: json) -> json:
     position_detections = []
     for building in buildings:
-        p = Positioning(building['raws'], building['sniffers'], building['areas'])
-        result = p.perform_xy()
-        result = p.assign_area(df=result) 
-        result  = p.return_json(result)
+        p = Positioning(building=building)
+        xy_df = p.perform_xy()
+        xy_df = p.assign_area(df=xy_df)
+        print(xy_df)
+        
+        result  = fromDfToJson(xy_df)
         for detection in result:
             raw = [raw for raw in building['raws'] if raw['id'] == detection['id']][0]
             detection['id_building'] = raw['id_building']
             detection['timestamp'] = raw['timestamp']
         position_detections += result
+        
+    res = position_detections
     # res = post(f'{BASEDATA}/api/ai/create-position-detections/', json=position_detections)
-    # print(res.json())
+    # print(res.json())               
+    return res
+
+def main():
+    # BASEDATA = WIFEYE_BASEURL_STORAGE
+    # res = get(f'{BASEDATA}/api/details/ai/')
+    # buildings = res.json()
     
-    print(position_detections)
+    with open('./raw_data.json', 'r') as file:
+        buildings = json.load(file)
+    
+    json_result = jsonShit(buildings)
+    print(json_result)
 
 
 if __name__ == '__main__':
