a23b2c961943df7127e433fc528eb67f08b95043
Giuseppe Lombardi <g.lombardi11@studenti.unipi.it>
2022-11-24 15:55:17 +0100

    main fixed

--- main.py
+++ main.py
@@ -10,19 +10,16 @@ config = dotenv_values('env_file')
 WIFEYE_BASEURL_STORAGE = config['WIFEYE_BASEURL_STORAGE']
 CRON_SECONDS = int(config['CRON_SECONDS'])
 
-def main():
-    BASEDATA = WIFEYE_BASEURL_STORAGE
-    res = get(f'{BASEDATA}/api/details/ai/')
-    buildings = res.json()
-    # import json
-    # with open('./raw_data.json', 'r') as file:
-    #     buildings = json.load(file)
+def fromDfToJson(df: pd.DataFrame) -> json:
+    df["id"] = df.index
+    return df.to_dict(orient="records")
+
+def jsonShit(buildings: json) -> json:
     position_detections = []
     for building in buildings:
         p = Positioning(building=building)
         xy_df = p.perform_xy()
         xy_df = p.assign_area(df=xy_df)
-        # print(xy_df)
         
         result  = fromDfToJson(xy_df)
         for detection in result:
@@ -30,15 +27,27 @@ def main():
             detection['id_building'] = raw['id_building']
             detection['timestamp'] = raw['timestamp']
         position_detections += result
-    res = post(f'{BASEDATA}/api/ai/create-position-detections/', json=position_detections)
-    print(res.json())
+        
+    res = position_detections
+    # res = post(f'{BASEDATA}/api/ai/create-position-detections/', json=position_detections)
+    # print(res.json())               
+    return res
+
+def main():
+    # BASEDATA = WIFEYE_BASEURL_STORAGE
+    # res = get(f'{BASEDATA}/api/details/ai/')
+    # buildings = res.json()
     
-    # print(position_detections)
+    with open('./raw_data.json', 'r') as file:
+        buildings = json.load(file)
+    
+    json_result = jsonShit(buildings)
+    # print(json_result)
 
 
 if __name__ == '__main__':
-    # main()
-    schedule.every(CRON_SECONDS).seconds.do(main)
-    while 1:
-        schedule.run_pending()
-        sleep(1)
+    main()
+    # schedule.every(CRON_SECONDS).seconds.do(main)
+    # while 1:
+    #     schedule.run_pending()
+    #     sleep(1)
